06779bad546292d4a467136ad5e273c09664e95e
Vlad Vartolomei <vlad.vartolomei@gmail.com>
2020-07-16 16:34:07 +0300

    account creation with 0 balance

--- src/test/java/org/xpdojo/bank/AccountTest.java
+++ src/test/java/org/xpdojo/bank/AccountTest.java
@@ -8,8 +8,8 @@ import static org.assertj.core.api.Assertions.assertThat;
 public class AccountTest {
 
     @Test
-	@Disabled
-    public void depositAnAmountToIncreaseTheBalance() {
-        assertThat("your first test").isBlank();
+    public void newAccountShouldHaveZeroBalance() {
+        Account account = new Account();
+        assertThat(account.balance()).isEqualTo(0);
     }
 }
